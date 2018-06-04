789f1f9cad39003a8eddcf9b70ef287184bf56c8
gzoritchak <g.zoritchak@gmail.com>
2018-06-04 11:59:11 +0200

    Adding style to sample.

--- sample/src/main/kotlin/Main.kt
+++ sample/src/main/kotlin/Main.kt
@@ -1,9 +1,28 @@
 import google.maps.LatLng
+import google.maps.Map
+import io.data2viz.google.maps.Visibility
+import io.data2viz.google.maps.styles
 import kotlin.browser.document
 
 fun main(args: Array<String>) {
     println("starting Google Map wrapped from kotlin")
-    val mOptions = mapOptions(6, LatLng(46.2, 6.15))
-    val mapDiv = document.getElementById("map")!!
-    google.maps.Map(mapDiv, mOptions)
+    val map = Map(
+        document.getElementById("map")!!, 
+        mapOptions(6, LatLng(46.2, 6.15)) {
+            styles = styles {
+                style {
+                    visibility = Visibility.off
+                }
+                style( administrative.country, geometry.stroke) {
+                    visibility = Visibility.on
+                    color = "#3e3366"
+                }
+                style( water, geometry) {
+                    visibility = Visibility.on
+                    color = "#b3b3b3"
+                }
+            }
+        }
+    )
+    map.setZoom(3)
 }
