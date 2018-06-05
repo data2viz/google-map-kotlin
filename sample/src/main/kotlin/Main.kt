3e8f82c37bfd2b332c271bf168c8695c06397fd7
gzoritchak <g.zoritchak@gmail.com>
2018-06-05 16:18:54 +0200

    More complete sample.

--- sample/src/main/kotlin/Main.kt
+++ sample/src/main/kotlin/Main.kt
@@ -1,38 +1,33 @@
 import google.maps.LatLng
 import google.maps.Map
+import google.maps.MapTypeId
 import io.data2viz.google.maps.Visibility
+import io.data2viz.google.maps.map
 import io.data2viz.google.maps.styles
 import kotlin.browser.document
 
 fun main(args: Array<String>) {
-    println("starting Google Map wrapped from kotlin")
-    val map = Map(
-        document.getElementById("map")!!, 
-        mapOptions(6, LatLng(46.2, 6.15)) {
-            styles = styles {
-                style {
-                    visibility = Visibility.off
-                }
-                style( administrative.country, geometry.stroke) {
-                    visibility = Visibility.on
-                    color = "#3e3366"
-                }
-                style( water, geometry) {
-                    visibility = Visibility.on
-                    color = "#b3b3b3"
-                }
+    map(
+        element = document.getElementById("map")!!, 
+        zoom = 7, 
+        center = LatLng(46.2, 6.15)) {
+        
+        minZoom = 3
+        
+        styles = styles { 
+            style { 
+                visibility = Visibility.off 
+            }
+            style(administrative.country) {
+                visibility = Visibility.on
+            }
+            style(administrative.country, geometry.stroke) {
+                color = "#0000aa"
+            }
+            style(water) {
+                visibility = Visibility.on
             }
         }
-    )
-    map.setZoom(3)
-
-    
-}
-
-private fun addHeatmapLayer(map: Map) {
-    val heatmapLayer = heatmapLayer {
-        this.map = map
-    }
-
-    heatmapLayer.setData(arrayOf(LatLng(46.2, 6.15)))
+        
+    } 
 }
